1497ba3efba425f3dbe7f4afd21b1800079d77a1
Kiran <kiran.kumal714@gmail.com>
2024-09-15 16:21:10 +0545

    fix: update datepicker style

--- src/components/ui/datepicker/datepicker.tsx
+++ src/components/ui/datepicker/datepicker.tsx
@@ -17,7 +17,7 @@ export function DatePicker({
         months:
           "flex flex-row gap-7 mb-5 sm:flex-row space-y-4 sm:space-x-2 sm:space-y-0 relative",
         month:
-          "space-y-4 w-full [&>table]:w-full [&>table]:min-w-[19rem] [&>table]:flex [&>table]:flex-col [&>table>thead]:py-3 [&>table>thead>tr]:flex [&>table>thead>tr]:justify-between [&>table>thead>tr]:p-2 [&>table>tbody]:flex [&>table>tbody]:flex-col [&>table>tbody]:gap-4 [&>table>tbody>tr>td]:w-full [&>table>tbody>tr>td>button]:h-full [&>table>tbody>tr>td>button]:w-full [&>table>tbody>tr]:flex",
+          "space-y-4 w-full [&>table]:w-full [&>table]:min-w-[20rem] [&>table]:flex [&>table]:flex-col [&>table>thead]:py-3 [&>table>thead>tr]:flex [&>table>thead>tr]:justify-between [&>table>thead>tr]:p-2 [&>table>tbody]:flex [&>table>tbody]:flex-col [&>table>tbody]:gap-4 [&>table>tbody>tr>td]:w-full [&>table>tbody>tr>td>button]:h-full [&>table>tbody>tr>td>button]:w-full [&>table>tbody>tr]:flex",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-gray-700 text-base leading-6 font-bold",
         nav: "space-x-1 absolute right-0 flex items-center",
@@ -29,23 +29,21 @@ export function DatePicker({
         table: "!w-full border-collapse space-y-1",
         weekdays: "[&>th]:!text-base [&>th]:!font-medium",
         head_row: "flex",
-        head_cell:
-          "text-gray-700 w-9 font-semibold text-xs dark:text-slate-400",
+        head_cell: "text-gray-700 w-9 font-semibold text-xs",
         row: "flex w-full mt-2 w-full",
-        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected])]:bg-transparent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20 dark:[&:has([aria-selected].day-outside)]:bg-slate-800/50 dark:[&:has([aria-selected])]:bg-slate-800",
+        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected])]:bg-transparent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
-          "h-10 w-10 text-center text-gray-900 p-0 font-medium text-base leading-4 not-italic aria-selected:opacity-100 aria-selected:text-white hover:bg-blue-700 hover:rounded-md hover:text-white"
+          "h-10 w-10 text-center text-blue-900 aria-selected:text-blue-900 p-0 font-medium text-base leading-4 not-italic hover:bg-blue-700 hover:rounded-md hover:text-white"
         ),
-        range_end: "day-range-end",
-        selected: "bg-blue-700 text-white rounded-md",
-        // today:
-        //   "text-blue-700 text-slate-900 !bg-blue-400 dark:bg-slate-800 dark:text-slate-50",
-        disabled: "text-slate-500 opacity-50 dark:text-slate-400",
+        range_end: "",
+        selected: "bg-blue-700 !text-white rounded-md",
+        today: "bg-blue-100 rounded-md font-black hover:!text-slate-50",
+        disabled: "text-slate-500 opacity-50",
         range_middle:
-          "!bg-blue-100 !text-black/60 aria-selected:bg-slate-100 aria-selected:text-slate-900 dark:aria-selected:bg-slate-800 dark:aria-selected:text-slate-50 !rounded-none",
+          "!bg-blue-100 aria-selected:!bg-blue-100 !text-black aria-selected:!text-black hover:!text-black !rounded-none",
         day_hidden: "invisible",
         outside:
-          "text-slate-500 opacity-50 aria-selected:text-slate-500 aria-selected:opacity-30 dark:text-slate-400 dark:aria-selected:bg-slate-800/50 dark:aria-selected:text-slate-400",
+          "!text-slate-500 aria-selected:!text-slate-500 hover:!text-black/80 !opacity-50 aria-selected:!opacity-50 !bg-transparent",
 
         ...classNames,
       }}
